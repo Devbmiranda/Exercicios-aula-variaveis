537514c9be2b8b30c77d613c37e2c57a4bc617e6
Bruno <Bruno@MacBook-Pro.local>
2024-01-31 20:59:04 -0600

    correcao var para let

--- desconto-tenis.js
+++ desconto-tenis.js
@@ -1,9 +1,9 @@
-/ Declarando uma variável de desconto para tênis
-var descontoTenis = 0.2; // 20% de desconto
+// Declarando uma variável de desconto para tênis
+let descontoTenis = 0.2; // 20% de desconto
 
 // Exemplo de uso da variável de desconto
-var precoOriginal = 100; // Preço original do tênis
-var precoComDesconto = precoOriginal - (precoOriginal * descontoTenis);
+let precoOriginal = 100; // Preço original do tênis
+let precoComDesconto = precoOriginal - (precoOriginal * descontoTenis);
 
 console.log("Preço original: $" + precoOriginal);
 console.log("Desconto aplicado: " + (descontoTenis * 100) + "%");
